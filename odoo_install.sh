571bcb8b509a0814d27413fda865da6d5fcec127
Mehedi Hasan <git.mehedi@gmail.com>
2020-02-07 12:28:26 +0600

    [MOD]
    change

--- odoo_install.sh
+++ odoo_install.sh
@@ -203,7 +203,7 @@ sudo update-rc.d $OE_CONFIG defaults
 
 echo -e "* Starting Odoo Service"
 sudo su root -c "/etc/init.d/$OE_CONFIG start"
-echo "-----------------------------------------------------------"
+echo "---------------------------------------------------------"
 echo "Done! The Odoo server is up and running. Specifications:"
 echo "Port: $OE_PORT"
 echo "User service: $OE_USER"
